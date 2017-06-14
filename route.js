2fb9ac9fb71ec2c62532e6fe1accdac1f2a0da84
Austin Wright <aaa@bzfx.net>
2017-06-13 22:25:07 -0700

    Another near rewrite

--- route.js
+++ route.js
@@ -20,13 +20,11 @@ function Node(){
 	// If in an expression, this is potentially the end of the expression
 	this.chr = {};
 	// Expressions to decend into
-	this.exp = [];
-	// If we're currently in an expression...
-	this.exp_label = null;
+	this.exp_match = [];
+	// If we're currently in an expression
 	this.exp_info = null;
 	this.exp_range = null;
-	this.exp_match = [];
-	this.exp_end = null;
+	this.exp_chr = {};
 }
 
 function Route(template, arg){
@@ -97,67 +95,53 @@ Router.prototype.addTemplate = function addTemplate(uri, variables, arg){
 			var prefix = modifier.prefix;
 			var prefixNext = modifier.prefixNext;
 			var range = modifier.range;
-			var repeats = 0;
-			var variableList = patternBody
-				.substring(modifierChar.length)
-				.split(/,/g)
-				.map(function(varspec, index){
-					if(varspec.match(/\*$/)){
-						if(!prefixNext){
-							throw new Error('Variable modifier '+JSON.stringify(modifier)+' does not work with explode modifier');
-						}
-						var varname = varspec.substring(0, varspec.length-1);
-						var explode = true;
-					}else{
-						var varname = varspec;
-						var explode = false;
+			patternBody
+			.substring(modifierChar.length)
+			.split(/,/g)
+			.map(function(varspec, index){
+				if(varspec.match(/\*$/)){
+					if(!prefixNext){
+						throw new Error('Variable modifier '+JSON.stringify(modifier)+' does not work with explode modifier');
 					}
-					return {
-						varname: varname,
-						explode: explode,
-					};
-				});
-			var variableInfo = {};
-			variableList.forEach(function(varspec, index){
+					var varname = varspec.substring(0, varspec.length-1);
+					var explode = true;
+				}else{
+					var varname = varspec;
+					var explode = false;
+				}
+				return {
+					varname: varname,
+					prefix: index ? prefixNext : prefix,
+					range: modifier.range,
+					explode: explode,
+				};
+			})
+			.forEach(function(varspec, index){
 				var varname = varspec.varname;
 				var explode = varspec.explode;
-				repeats++;
-				if(explode) explode = Math.POSITIVE_INFINITY;
 				if(varnames[varname]){
 					throw new Error('Variable '+JSON.stringify(varname)+' is already used');
 				}
-				varnames[varname] = varspec;
+				var expressionInfo = {};
+				varspec.index = Object.keys(varnames).length;
+				varnames[varspec.varname] = varspec;
+
+				var end = new Node;
+				if(prefix){
+					node.exp_chr[prefix] = node.exp_chr[prefix] || new Node;
+					node = node.exp_chr[prefix];
+				}
+				node.exp_info = {type:'EXP', index:varspec.index};
+				node.exp_range = node.exp_range || {};
+				node.exp_range[range] = node.exp_range[range] || new Node;
+				node = node.exp_range[range];
 			});
-			var exprinfo = new Node;
-			var end = new Node;
-			if(prefix){
-				exprinfo.exp_label = patternBody+' prefix';
-				exprinfo.exp_range = {};
-				exprinfo.exp_info = {prefix:prefix};
-				exprinfo.exp_range[prefix] = null;
-				var e2 = exprinfo.exp_match = new Node;
-				exprinfo.exp_end = end;
-			}else{
-				var e2 = exprinfo;
-			}
-			e2.exp_info = {name:patternBody};
-			e2.exp_label = patternBody+' body';
-			e2.chr = end.chr;
-			//e2.exp_range = RANGES_MAP[range];
-			e2.exp_range = range;
-			e2.exp_match = e2;
-			if(repeats>=Math.POSITIVE_INFINITY){
-				e2.exp_end = exprinfo
-			}else{
-				e2.exp_end = end;
-			}
-			node.exp.push(exprinfo);
-			node = end;
 		}else{
 			// Decend node into the branch, creating it if it doesn't exist
 			// if chr is undefined, this will set the key "undefined"
 			node.chr[chr] = node.chr[chr] || new Node;
 			node = node.chr[chr];
+			node.chr_offset = uri_i;
 		}
 	}
 	if(node.end){
@@ -169,35 +153,31 @@ Router.prototype.addTemplate = function addTemplate(uri, variables, arg){
 function StateSet(offset, tier, alternatives){
 	this.offset = offset;
 	this.tier = tier;
-	// A list of equal alternatives
+	// A list of equal alternative candidates for processing this input character
 	this.alts = alternatives;
 }
 
 var S = {
 	EOF: 10,
-	LIT: 20,
-	LITPCT0: 21,
-	LITPCT1: 22,
-	LITPCT3: 23,
-	PFX: 30,
-	EXP: 40,
-	EXPPCT0: 41,
-	EXPPCT0: 42,
-	EXPPCT0: 43,
+	CHR: 20,
+	PCT1: 31,
+	PCT2: 32,
 };
 // Enable for testing
 for(var n in S) S[n]=n;
 
-function State(prev, offset, branch, state, data){
+function State(prev, offset, branch, mode, data){
+	if(prev && !(prev instanceof State)) throw new Error('prev not instanceof State');
+	if(!(branch instanceof Node)) throw new Error('branch not instanceof Node');
 	this.prev = prev;
 	this.offset = offset;
 	this.branch = branch;
-	this.state = state;
+	this.mode = mode;
 	this.data = data;
 }
-State.prototype.push = function push(offset, branch, state, data){
+State.prototype.push = function push(offset, branch, mode, data){
 	log('Decend', offset, data);
-	return new State(this, offset, branch, state, data);
+	return new State(this, offset, branch, mode, data);
 }
 
 function withChange(bindings, name, value){
@@ -220,74 +200,56 @@ function withChange(bindings, name, value){
 
 Router.prototype.resolveURI = function resolve(uri, flags){
 	var parse_backtrack = [
-		new StateSet(0, 0, [new State(null, 0, this.tree)]),
+		new StateSet(0, 0, [new State(null, 0, this.tree, S.CHR)]),
 	];
+	function consumeInputCharacter(offset, chr, state, branch){
+		if(!(branch instanceof Node)) throw new Error('branch not instanceof Node');
+		var mode = state.mode;
+		log('branch', chr, offset, branch);
+		if(branch.end){
+			return finish(state.end, state.push(offset, branch.end, S.EOF, 'end'));
+		}
+		if(branch.chr[chr]){
+			log('chr', chr);
+			parse_chr.alts.push(state.push(offset, branch.chr[chr], S.CHR, 'chr'));
+		}
+		if(branch.exp_chr[chr]){
+			log('prefix', chr);
+			parse_chr.alts.push(state.push(offset, branch.exp_chr[chr], S.CHR, 'exp_chr'));
+		}
+		for(var rangeName in branch.exp_range){
+			log('exp_match', rangeName);
+			var validRange = RANGES_MAP[rangeName];
+			var exprInfo = branch.exp_range[rangeName];
+			if(chr in validRange){
+				parse_exp.alts.push(state.push(offset, exprInfo, S.CHR, branch.exp_info));
+			}else{
+				log('exp_end');
+				consumeInputCharacter(offset, chr, state, exprInfo.end);
+			}
+		}
+	}
 	for(var offset=0; offset<=uri.length; offset++){
 //		log('Offset=%d Stack=%d', offset, parse_backtrack.length);
 		var stateset_this = parse_backtrack.pop();
 		//log(stateset_this);
 		if(!stateset_this) break;
 		offset = stateset_this.offset;
-		var parse_chr = new StateSet(offset+1, 2, []);
+		var parse_chr = new StateSet(offset+1, 3, []);
+		var parse_pfx = new StateSet(offset+1, 2, []);
 		var parse_exp = new StateSet(offset+1, 1, []);
 		var chr = uri[offset];
 		log('Parse('+offset+') '+chr);
 		for(var alt_i=0; alt_i<stateset_this.alts.length; alt_i++){
 			var alt = stateset_this.alts[alt_i];
-			log('Alt', alt)
-			var node = alt.branch;
-//			log(node);
-			if(node.end){
-				return finish(node.end, alt.push(offset, node.end, S.EOF, 'end'));
-			}
-			if(node.chr[chr]){
-				log('chr');
-				parse_chr.alts.push(alt.push(offset, node.chr[chr], S.LIT, 'chr'));
-			}
-			if(node.exp_range){
-				// If we're currently in an expression
-				var validRange = typeof node.exp_range==='string' ? RANGES_MAP[node.exp_range] : node.exp_range ;
-				if(chr in validRange){
-					log('exp_match');
-					if(node.exp_match) parse_exp.alts.push(alt.push(offset, node.exp_match, S.EXP, node.exp_match.exp_info));
-				}else if(node.exp_end){
-					log('exp_end');
-					// Exit expression and evaluate end-of-expression condition
-					if(node.exp_end.chr[chr]){
-						parse_chr.alts.push(alt.push(offset, node.exp_end.chr[chr], S.LIT, 'chr'));
-					}
-					node.exp_end.exp.forEach(function(exp){
-						log('exp_end exp');
-						var validRange = typeof exp.exp_range==='string' ? RANGES_MAP[exp.exp_range] : exp.exp_range ;
-						if(chr in validRange){
-							parse_exp.alts.push(
-								alt.push(offset, exp.exp_match, S.EXP, exp.exp_match.exp_info)
-							);
-						}else{
-							parse_exp.alts.push(
-								alt.push(offset, exp.exp_end, E.LIT, exp.exp_end.exp_info)
-							);
-						}
-					});
-				}else{
-					log('no exp_end');
-				}
-			}else{
-				log('exp_start');
-				// If no expression
-				node.exp.forEach(function(exp){
-					// Test that potential expression matches current character
-					var validRange = typeof exp.exp_range==='string' ? RANGES_MAP[exp.exp_range] : exp.exp_range ;
-					if(chr in validRange){
-						parse_exp.alts.push(
-							alt.push(offset, exp.exp_match, S.EXP, exp.exp_match.exp_info)
-						);
-					}
-				});
-			}
+			consumeInputCharacter(offset, chr, alt, alt.branch);
 		}
+		// Push expressions onto the stack, below (before) matched characters however
 		if(parse_exp.alts.length){
 			parse_backtrack.push(parse_exp);
+		}
+		if(parse_pfx.alts.length){
+			parse_backtrack.push(parse_pfx);
 		}
 		// Push highest priority stuff to top of stack (i.e. last)
 		if(parse_chr.alts.length){
@@ -311,7 +273,7 @@ Router.prototype.resolveURI = function resolve(uri, flags){
 	function finish(route, match){
 		var history = [];
 		for(var item=solution; item.prev; item=item.prev){
-			history.unshift({offset:item.offset, state:item.state, data:item.data, chr:uri[item.offset]});
+			history.unshift({offset:item.offset, mode:item.mode, data:item.data, chr:uri[item.offset]});
 		}
 		log('history');
 		log(history);
