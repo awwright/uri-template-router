0c9f5a2ecf2bbcff263f6e71668948f4ee86ac11
Austin Wright <aaa@bzfx.net>
2018-10-02 18:28:02 -0700

    Iterate through URI Template tests (if an array) for better assert errors

--- test/uritemplate-test.js
+++ test/uritemplate-test.js
@@ -1,6 +1,7 @@
 
 var fs = require('fs');
 var assert = require('assert');
+var inspect = require('util').inspect;
 var R = require('..');
 
 var files = [
@@ -17,12 +18,16 @@ files.forEach(function(filename){
 			var section = data[sectionName];
 			describe(sectionName, function(){
 				section.testcases.forEach(function(test){
+//					it(test[0]+' <- '+inspect(section.variables).replace(/\s+/g,' '), function(){
 					it(test[0], function(){
-						var uri = new R.Route(test[0]).gen(section.variables);
-						if(Array.isArray(test[1])){
-							assert(test[1].indexOf(uri) >= 0);
+						var expected = test[1];
+						var actual = new R.Route(test[0]).gen(section.variables);
+						if(Array.isArray(expected)){
+							for(var i=0; i<expected.length && actual!=expected[i]; i++);
+							if(i==expected.length) i=0;
+							assert.equal(actual, expected[i]);
 						}else{
-							assert.equal(uri, test[1]);
+							assert.equal(actual, expected);
 						}
 					});
 				});
