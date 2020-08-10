e4552708a044450c52d2cd395e699b04ac5a98a8
Austin Wright <aaa@bzfx.net>
2020-08-09 22:21:58 -0700

    test: nest syntax tests in describe

--- test/syntax.test.js
+++ test/syntax.test.js
@@ -8,21 +8,23 @@ var Router = require('..').Router;
 var r = new Router;
 
 var tests = require('./base.json');
-tests.forEach(function(testPage){
-	describe(testPage.label, function(){
-		var router = new Router;
-		testPage.templates.forEach(function(template, i){
-			router.addTemplate(template, {}, i);
-		});
-		Object.keys(testPage.uris).forEach(function(uri){
-			it(uri, function(){
-				var route = router.resolveURI(uri);
-				var expected = testPage.uris[uri];
-				if(route && route.params){
-					assert.deepEqual(route.params, expected);
-				}else{
-					assert(!expected);
-				}
+describe('base.json', function(){
+	tests.forEach(function(testPage){
+		describe(testPage.label, function(){
+			var router = new Router;
+			testPage.templates.forEach(function(template, i){
+				router.addTemplate(template, {}, i);
+			});
+			Object.keys(testPage.uris).forEach(function(uri){
+				it(uri, function(){
+					var route = router.resolveURI(uri);
+					var expected = testPage.uris[uri];
+					if(route && route.params){
+						assert.deepEqual(route.params, expected);
+					}else{
+						assert(!expected);
+					}
+				});
 			});
 		});
 	});
